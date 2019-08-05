4dccab79c143f5ca7d1c06fa4b8c279b23d06769
James Gibbel <jamesgibbel@gmail.com>
2019-08-05 22:49:56 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,31 @@
+def using_push(a,s)
+  a.push(s)
+end
+
+def using_unshift(a,s)
+  a.unshift(s)
+end
+
+def using_pop(a)
+  a.pop 
+end
+
+def pop_with_args(a)
+  a.pop(2)
+end
+
+def using_shift(a)
+  a.shift 
+end
+
+def shift_with_args(a)
+  a.shift(2)
+end
+
+def using_concat(a, b)
+  a.concat(b)
+end
+
+def using_insert(a, e)
+  a.insert
+  
